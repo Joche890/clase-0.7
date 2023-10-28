9d92d3cbd7ec97a0c983a59bcccef1a53d4a00af
Jose Daniel Aira <danielbravo891@gmail.com>
2023-10-27 23:28:29 -0300

    añadiendo mis primeros trabajos a git

---
+++ js/scrip.js
@@ -0,0 +1,19 @@
+
+
+/* let numero = Number(prompt("ingrese un numero")) */
+
+//parseint,parsefloat,number
+
+
+
+const nombres =["mariano","gaston","pepe","maria"]
+/* for(let i = 0; i < nombres.length, i = i + 1) */
+/* for(let i = 0; i < nombres.length; i++){
+    console.log(nombres[i])
+}
+ */
+
+//for..of
+for(let nombre of nombres){
+    console.log(nombre)
+}
